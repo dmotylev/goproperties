e028889712623e2dff3dc89b10c2f0f4f8b99ca2
Dmitry Motylev <dmotylev@dmotylev.(none)>
2009-11-29 00:06:43 +0300

    set up tests for all test properties

--- properties_test.go
+++ properties_test.go
@@ -19,7 +19,20 @@ func TestLoad(t *testing.T) {
 	if loadErr != nil {
 		t.Fatalf("failed to load %s: %s", _testFilename, loadErr)
 	}
-	//for i := 0; i < len(props); i++ {
-	t.Logf("%v\n", props);
-	//}
+
+	if props["website"] != "http://en.wikipedia.org/" {
+		t.Error("website")
+	}
+	if props["language"] != "English" {
+		t.Error("language")
+	}
+	if props["message"] != "Welcome to Wikipedia!" {
+		t.Error("message")
+	}
+	if props["unicode"] != "\u041f\u0440\u0438\u0432\u0435\u0442, \u0421\u043e\u0432\u0430!" {
+		t.Error("unicode")
+	}
+	if props["key with spaces"] != "This is the value that could be looked up with the key \"key with spaces\"." {
+		t.Error("key with spaces")
+	}
 }
